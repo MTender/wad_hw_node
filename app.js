8db08e49d97b20802d76e0554573fc0af84f635e
Märt <mart.tender@ut.ee>
2021-11-24 10:15:54 +0200

    Change post structure

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('Public'));
+app.use(express.static('public'));
 
 app.listen(3000);
 
